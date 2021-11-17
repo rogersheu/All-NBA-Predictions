7f927d1fdf43597c16a4b5e582caeb632bfff102
rogersheu <rogersheu92@gmail.com>
2021-11-16 17:57:50 -0800

    Starting code for machine learning predictions of 2021-22 NBA All-Stars

---
+++ scrapinghistoricaldata.py
@@ -0,0 +1,9 @@
+# Scraping all season total data from the 1979-1980 season to the current season.
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+import numpy as np
+import datetime
+import pandas as pd
+
