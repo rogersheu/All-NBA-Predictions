368b41f03d34e92ce25174755bc93386e843326a
rogersheu <rogersheu92@gmail.com>
2021-11-17 16:57:27 -0800

    Ran black, small changes.

--- scripts/scrape_stats.py
+++ scripts/scrape_stats.py
@@ -7,6 +7,7 @@ from csv_functions import write_to_csv
 from csv_functions import reset_csv
 import unicodedata
 import os
+
 # import re
 
 # tablePattern = re.compile(r"(full_table|italic-text partial-table")
@@ -31,7 +32,7 @@ def get_singleseason_stats(year, URL, fileName):
 
         write_to_csv(fileName, header)
 
-        dataList = dataTable.find_all('tr', class_ = ["full_table", "italic_text"])
+        dataList = dataTable.find_all("tr", class_=["full_table", "italic_text"])
         # totalsDataList = totalsTable.find_all('tr')
 
         # Pull players who got changed teams mid-season and just do a check for
@@ -54,19 +55,19 @@ def get_singleseason_stats(year, URL, fileName):
         return False
 
 
-
 def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    nfkd_form = unicodedata.normalize("NFKD", input_str)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
 
 # Goes from 1979-1980 to 2021-2022 (current season)
 def get_each_season_totals():
     print("Do you want (A) Total Stats, (B) Advanced Stats, or (C) Play-by-Play Stats?")
     statType = input("A, B, or C?  ")
-    
-    if statType not in ['A', 'B', 'C']:
+
+    if statType not in ["A", "B", "C"]:
         print("Please enter a valid letter.")
-    
+
     print("Please provide the first year for which you want data.")
     print("For a single season, enter the same number for both.")
     print("Example 1: Entering 2020 and 2020 gives the 2019-2020 season.")
@@ -86,7 +87,6 @@ def get_each_season_totals():
         print("Please enter valid years.")
         return False
 
-
     if (yearStart or yearEnd) < 1979:
         print("Warning. Picking seasons before the 1979-1980 season will skew data because of the introducton of the 3 point line in 1979.")
         return False
@@ -95,34 +95,49 @@ def get_each_season_totals():
         print("Please pick an ending year AFTER your starting year.")
         return False
 
-    if statType == 'C' and yearStart < 1997:
+    if statType == "C" and yearStart < 1997:
         print("Play-by-play data only started in the 1996-1997 season.")
         return False
 
     else:
-        yearList = range(yearStart, yearEnd+1)
+        yearList = range(yearStart, yearEnd + 1)
         yearList = [num for num in yearList]
         for year in yearList:
-            if statType == 'A':
+            if statType == "A":
                 typeKey = "totals"
 
-            elif statType == 'B':
+            elif statType == "B":
                 typeKey = "advanced"
 
-            elif statType == 'C':
+            elif statType == "C":
                 typeKey = "play-by-play"
 
-            URL = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_" + typeKey + ".html"
+            URL = (
+                "https://www.basketball-reference.com/leagues/NBA_"
+                + str(year)
+                + "_"
+                + typeKey
+                + ".html"
+            )
 
             mkdir = "baseData/" + typeKey
             if not os.path.exists(mkdir):
                 os.makedirs(mkdir)
-            
-            fileName = "baseData/" + typeKey + "/" + typeKey + "_stats_" + str(year-1) + "_" + str(year) + ".csv"
+
+            fileName = (
+                "baseData/"
+                + typeKey
+                + "/"
+                + typeKey
+                + "_stats_"
+                + str(year - 1)
+                + "_"
+                + str(year)
+                + ".csv"
+            )
 
             get_singleseason_stats(year, URL, fileName)
-            print("Finished populating season " + str(year-1) + "-" + str(year) + ".")
-
+            print("Finished populating season " + str(year - 1) + "-" + str(year) + ".")
 
 
 get_each_season_totals()
