54e84ba5d881cb3f45f5c8f20d914eaf3adb432e
rogersheu <rogersheu92@gmail.com>
2021-11-29 23:01:13 -0800

    Renaming file

--- scripts/daily_scrape.py
+++ scripts/daily_scrape.py
@@ -1,11 +1,12 @@
 from scrape_stats_cli import get_singleseason_stats, get_typeKey, years_areValid
 from csv_functions import make_dir_if_nonexistent, reset_csv
-from scrape_teamrecords import scrape_teamrecords
+from scrape_teamrecords import scrape_all_team_records
 from datetime import datetime
 from abbreviate_team_names import add_abbreviated_team_names
 
 
 shortDate = datetime.today().strftime('%Y-%m-%d')
+shortDate_nodash = shortDate.replace("-","")
 
 directory= "C:/Users/Roger/Documents/GitHub/All-Star-Predictions/baseData/dailystats"
 mkdir = (f"{directory}/{shortDate}")
@@ -19,19 +20,19 @@ def save_each_season_stats_daily(statType, yearStart, yearEnd):
 
         for year in yearList:
             URL = (f"https://www.basketball-reference.com/leagues/NBA_{year}_{typeKey}.html")
-            fileName = (f"{mkdir}/{typeKey}.csv") # f-stringed
+            fileName = (f"{mkdir}/{typeKey}_{shortDate_nodash}.csv") # f-stringed
             reset_csv(fileName)
             get_singleseason_stats(year, URL, fileName, True)
-            print(f"Finished populating season {year - 1}-{year}.") # f-stringed
+            print(f"Finished populating season {year - 1}-{year}, {typeKey} data.") # f-stringed
     else:
         return False
 
 
 def main():
-    save_each_season_stats_daily('-tot', 2022, 2022)
-    save_each_season_stats_daily('-adv', 2022, 2022)
-    scrape_teamrecords((f"{mkdir}/teamStandings.csv"))
-    add_abbreviated_team_names((f"{mkdir}/teamStandings.csv"))
+    save_each_season_stats_daily('-tot', '2022', '2022')
+    save_each_season_stats_daily('-adv', '2022', '2022')
+    scrape_all_team_records((f"{mkdir}/teamStandings_{shortDate_nodash}.csv"), 2022, 2022)
+    add_abbreviated_team_names((f"{mkdir}/teamStandings_{shortDate_nodash}.csv"))
 
 
 if __name__ == '__main__':
