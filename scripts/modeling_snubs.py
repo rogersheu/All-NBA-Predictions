eba933d46d6d13415868a4884c23059ad0f77044
rogersheu <rogersheu92@gmail.com>
2021-11-30 22:53:13 -0800

    Looked at allstar/allNBA snubs through these models.

---
+++ scripts/modeling_snubs.py
@@ -0,0 +1,23 @@
+from SVMmodeling import SVM
+from kNNmodeling import kNN
+from RFmodeling import RF
+from MLPmodeling import MLP
+from transfer_data import *
+
+
+def main():
+    X, y = get_all_player_stats()
+    print("Please pick the data file you want to append this to.")
+    srcFile = pick_file()
+    newFileName = srcFile.replace(".csv","")
+    newFileName = f"{newFileName}_snubs.csv"
+    df = pd.read_csv(srcFile)
+    df['RF'] = RF(X, y, X)
+    df['SVM'] = SVM(X, y, X)
+    df['kNN'] = kNN(X, y, X)
+    df['MLP'] = MLP(X, y, X)
+    df.to_csv(newFileName)
+    sortCSV_historical(newFileName)
+
+if __name__ == "__main__":
+    main()
