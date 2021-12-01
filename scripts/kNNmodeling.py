58c7019e806de82775151b18da55bce9089191fe
rogersheu <rogersheu92@gmail.com>
2021-11-30 22:55:09 -0800

    Small removal of an import

--- scripts/kNNmodeling.py
+++ scripts/kNNmodeling.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-# from sklearn.metrics import accuracy_score
 from transfer_data import *
 
 
