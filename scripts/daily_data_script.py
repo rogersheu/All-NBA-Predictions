a1523e67b77415e104ac1624681fe86ca5f8adbb
rogersheu <rogersheu92@gmail.com>
2021-12-02 15:42:32 -0800

    Added pace scrape

--- scripts/daily_data_script.py
+++ scripts/daily_data_script.py
@@ -1,9 +1,10 @@
+from tkinter.constants import S
 from scrape_stats_cli import get_singleseason_stats, get_typeKey, years_areValid
 from csv_functions import make_dir_if_nonexistent, reset_csv
 from scrape_teamrecords import scrape_all_team_records
 from datetime import datetime
 from abbreviate_team_names import add_abbreviated_team_names
-
+from scrape_advancedteamstats import scrape_alladvanced
 
 shortDate = datetime.today().strftime('%Y-%m-%d')
 shortDate_nodash = shortDate.replace("-","")
@@ -33,6 +34,7 @@ def main():
     save_each_season_stats_daily('-adv', '2022', '2022')
     scrape_all_team_records((f"{mkdir}/teamStandings_{shortDate_nodash}.csv"), 2022, 2022)
     add_abbreviated_team_names((f"{mkdir}/teamStandings_{shortDate_nodash}.csv"))
+    scrape_alladvanced('1980', '2022')
 
 
 if __name__ == '__main__':
