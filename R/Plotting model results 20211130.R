9e9adf40696635152e6ea219da73adbf7cfb7ee0
rogersheu <rogersheu92@gmail.com>
2021-12-02 15:42:17 -0800

    Implementing a function to output the desired figures

--- R/Plotting model results 20211130.R
+++ R/Plotting model results 20211130.R
@@ -1,26 +1,35 @@
 library(dplyr)
 library(ggplot2)
 
-stats_20211130_modeled <- read_csv("~/GitHub/All-Star-Predictions/baseData/dailystats/2021-11-30/stats_20211130_modeled.csv")
-df <- select(stats_20211130_modeled, Player, RF, SVM, kNN, MLP)
+# Write a function that passes in the date as an input.
 
-df$Avg <- rowMeans(subset(df, select = c(RF, SVM, kNN, MLP)), na.rm = TRUE)
+# Load relevant daily data
+# stats_20211130_modeled <- read_csv("~/GitHub/All-Star-Predictions/baseData/dailystats/2021-11-30/stats_20211130_modeled.csv")
+# df <- select(stats_20211130_modeled, Player, RF, SVM, kNN, MLP)
+stats_20211201_modeled <- read_csv("~/GitHub/All-Star-Predictions/baseData/dailystats/2021-12-01/stats_20211201_modeled.csv")
+df <- select(stats_20211201_modeled, Player, RF, SVM, kNN, MLP)
+
+df$Avg <- rowMeans(subset(df, select = c(RF, SVM, MLP)), na.rm = TRUE) # Removed kNN because it is consistently lower than the other models.
+# Perhaps that's indicating that I need to tune the hyperparameters on the other models?
 topCandidates <- filter(df, Avg > 0.25)
+attach(topCandidates)
+topCandidates <- topCandidates[order(-Avg),]
+detach(topCandidates)
 
 ggplot(topCandidates, aes(x=Player, y=RF)) + 
   theme_bw() + 
   theme(axis.text.y = element_text(face = "bold")) +
   ylab("Model Probability") +
-  xlab("Player Name") + 
+  xlab("") + 
   scale_y_continuous(breaks=seq(0,1,0.1)) +
   scale_x_discrete(limits = rev(c(topCandidates$Player))) +
   coord_flip() +
-  geom_point(aes(x=Player, y=RF, color = "RF"), position = position_jitter(w=.1), size = 3, alpha = 0.5) + 
-  geom_point(aes(x=Player, y=SVM, color = "SVM"), position = position_jitter(w=.1), size = 3, alpha = 0.5) + 
-  geom_point(aes(x=Player, y=kNN, color = "kNN"), position = position_jitter(w=.1), size = 3, alpha = 0.5) + 
-  geom_point(aes(x=Player, y=MLP, color = "MLP"), position = position_jitter(w=.1), size = 3, alpha = 0.5) +
-  geom_point(aes(x=Player, y=Avg, color = "Avg"), position = position_jitter(w=.1), size = 3, alpha = 0.5) +
-  scale_color_manual(values = c("RF" = "forestgreen", "SVM" = "goldenrod", "kNN" = "firebrick1", "MLP" = "royalblue", "Avg" = "gray24"))
+  geom_point(aes(x=Player, y=RF, color = "RF"), position = position_jitter(w=.1), size = 3, alpha = 0.25) + 
+  geom_point(aes(x=Player, y=SVM, color = "SVM"), position = position_jitter(w=.1), size = 3, alpha = 0.25) + 
+  geom_point(aes(x=Player, y=kNN, color = "kNN"), position = position_jitter(w=.1), size = 3, alpha = 0.25) + 
+  geom_point(aes(x=Player, y=MLP, color = "MLP"), position = position_jitter(w=.1), size = 3, alpha = 0.25) +
+  geom_point(aes(x=Player, y=Avg, color = "Avg"), size = 3, alpha = 1) +
+  scale_color_manual(values = c("RF" = "springgreen", "SVM" = "goldenrod", "kNN" = "firebrick1", "MLP" = "royalblue", "Avg" = "gray24"))
 
 
 
