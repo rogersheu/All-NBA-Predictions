612709e3f7f5cc6bdbf62c009958b4840ff8365a
rogersheu <rogersheu92@gmail.com>
2021-12-06 23:28:40 -0800

    Hyperparameter tuning tests

---
+++ scripts/kNNhyperparameter.py
@@ -0,0 +1,61 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from transfer_data import *
+from sklearn.model_selection import GridSearchCV
+
+
+def kNN(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # [n_neighbors, p] = 81, 1 best for precision
+    # [n_neighbors, p] = 5, 2 best for recall
+    parameter_space = { 
+        "n_neighbors" : list(range(1, 101, 1)),
+        "p" : [1, 2],
+    }
+    
+    kNNmodel = KNeighborsClassifier()
+
+    scores = ["precision", "recall"]
+
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = GridSearchCV(kNNmodel, parameter_space, scoring="%s_macro" % score, n_jobs = -1, cv = 3) 
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_["mean_test_score"]
+        stds = clf.cv_results_["std_test_score"]
+        for mean, std, params in zip(means, stds, clf.cv_results_["params"]):
+            print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
+
+def main():
+    X, y = get_all_player_stats()
+    kNN(X, y)
+
+if __name__ == "__main__":
+    main()
