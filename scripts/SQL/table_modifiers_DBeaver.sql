6e4386941991f8fa181ae63f5f763bf3474cdf2a
rogersheu <rogersheu92@gmail.com>
2021-12-15 15:23:16 -0800

    loading script

--- scripts/SQL/table_modifiers_DBeaver.sql
+++ scripts/SQL/table_modifiers_DBeaver.sql
@@ -6,12 +6,3 @@ DROP TABLE teamWL2022;
 
 DROP TABLE teamadv;
 
---------
--- Update tables now
---------
-
-ALTER TABLE advanced RENAME COLUMN "TS%" TO TS;
-ALTER TABLE advanced RENAME COLUMN "WS/48" TO WS48;
-ALTER TABLE teamStandingsAbbrev RENAME TO teamWL2022;
-ALTER TABLE teamadv_csv RENAME TO teamadv;
-ALTER TABLE teamadv RENAME COLUMN "TS%" TO TS;
