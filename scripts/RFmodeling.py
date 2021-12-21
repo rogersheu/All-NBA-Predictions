d1945a648a381017453763a76d8e70894c13a11d
rogersheu <rogersheu92@gmail.com>
2021-12-21 00:55:59 -0800

    Moved auxiliary modeling functions to their own place

--- scripts/RFmodeling.py
+++ scripts/RFmodeling.py
@@ -2,8 +2,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from transfer_data import *
-# from sklearn.calibration import calibration_curve
-# import matplotlib.pyplot as plt
+from sklearn.calibration import calibration_curve
+import matplotlib.pyplot as plt
 
 
 # Next thing to implement is GridSearchCV
@@ -23,20 +23,19 @@ def RF(X, y, X_2022): # Change to take in a csv and output a csv
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
 
-    
-    # y_means, proba_means = calibration_curve(y_pred, randomforest.predict_proba(X_test)[:,1], n_bins = 7, strategy = 'uniform')
-    # plt.plot([0, 1], [0, 1], linestyle = '--', label = 'Perfect calibration')
-    # plt.plot(proba_means, y_means)
-
-    # feature_imp = pd.Series(randomforest.feature_importances_,index=X.columns.values).sort_values(ascending=False)
-    # print(feature_imp)
-
-    # Making predictions
-    y_2022 = randomforest.predict(X_2022)
-
     predictions = randomforest.predict_proba(X_2022)
 
     return predictions[:,1]
+    
+
+def calcurve(randomforest : RandomForestClassifier, X_test, y_pred):
+    y_means, proba_means = calibration_curve(y_pred, randomforest.predict_proba(X_test)[:,1], n_bins = 7, strategy = 'uniform')
+    plt.plot([0, 1], [0, 1], linestyle = '--', label = 'Perfect calibration')
+    plt.plot(proba_means, y_means)
+
+def featureimportance(randomforest):
+    feature_imp = pd.Series(randomforest.feature_importances_,index=X.columns.values).sort_values(ascending=False)
+    print(feature_imp)
 
 def main():
     X, y = get_all_player_stats()
