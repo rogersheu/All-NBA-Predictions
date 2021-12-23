27ec0c4fc7041fbb9ea9f425a917e17b13124e8c
rogersheu <rogersheu92@gmail.com>
2021-12-22 16:36:16 -0800

    dropping extraneous table

--- scripts/SQL/auxiliary_functions.sql
+++ scripts/SQL/auxiliary_functions.sql
@@ -7,3 +7,4 @@ ALTER TABLE advanced RENAME COLUMN "WS/48" TO WS48;
 ALTER TABLE teamStandingsAbbrev RENAME TO teamWL2022;
 ALTER TABLE teamadv_csv RENAME TO teamadv;
 ALTER TABLE teamadv RENAME COLUMN "TS%" TO TS;
+DROP TABLE teamStandings;
