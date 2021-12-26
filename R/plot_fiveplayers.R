c288df7b9f04ad1116a5666b5cf1709ccbf80817
rogersheu <rogersheu92@gmail.com>
2021-12-25 23:37:14 -0800

    renamed function and variables for flexibility/clarity

--- R/plot_fiveplayers.R
+++ R/plot_fiveplayers.R
@@ -1,7 +1,8 @@
-plot_fiveplayers <- function(startIndex, endIndex, fileIndex) {
-  bestAvg <- bestAvg[startIndex:endIndex,]
-  topten <- c(bestAvg[startIndex:endIndex,1])
-  melted_players <- melt(bestAvg, id='Player')
+plot_Nplayers <- function(startIndex, endIndex) {
+  bestAvg_subset <- bestAvg[startIndex:endIndex,]
+  top_picks <- factor(c(bestAvg[startIndex:endIndex,1]), levels = c(bestAvg[startIndex:endIndex,1])) #automatically alphabetizes so you need to avoid it
+  
+  melted_players <- melt(bestAvg_subset, id='Player')
   lineplot <- ggplot(data=melted_players, aes(x=as.Date(variable), y=value, color=Player, group=Player)) + 
     geom_line(size=1) + 
     geom_point(size=3) +
