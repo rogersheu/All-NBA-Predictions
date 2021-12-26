4aa2a723ddd89ba651fdf8cf57ae6e9e12d247aa
rogersheu <rogersheu92@gmail.com>
2021-12-26 00:33:11 -0800

    45 degree x axis labels

--- R/plot_Nplayers.R
+++ R/plot_Nplayers.R
@@ -10,6 +10,7 @@ plot_Nplayers <- function(startIndex, endIndex) {
     theme(panel.grid.minor.x = element_blank()) +
     theme(panel.grid.minor.y = element_blank()) +
     xlab("Date") + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
     ylim(0,1) + 
     ylab("Avg Probability") +
     scale_x_date(date_breaks = "3 days") + 
