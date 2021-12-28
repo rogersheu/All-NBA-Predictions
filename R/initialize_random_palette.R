7133d47e69b5ad5fbb40d04c2dabc3018bf38dc1
rogersheu <rogersheu92@gmail.com>
2021-12-27 23:37:58 -0800

    Create initialize_random_palette.R

---
+++ R/initialize_random_palette.R
@@ -0,0 +1,10 @@
+palette_initialization <- function() {
+  n <- 60
+  #qual_col_pals <- brewer.pal.info[brewer.pal.info$category == 'qual',]
+  #col_vector <- unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  palette <- distinctColorPalette(n)
+  col_vector <- unname(distinctColorPalette(n))
+  
+  return(col_vector)
+  #pie(rep(1,n), col=sample(col_vector, n))
+}
