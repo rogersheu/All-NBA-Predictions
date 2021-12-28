c1b52cac29f04a34ad1fd5d62f96e58de94918bc
rogersheu <rogersheu92@gmail.com>
2021-12-27 23:48:33 -0800

    Added comments

--- R/plot_Nplayers.R
+++ R/plot_Nplayers.R
@@ -2,18 +2,19 @@ library(directlabels)
 library(scales)
 library(RColorBrewer)
 library(randomcoloR)
+library(reshape2)
 
 plot_Nplayers <- function(data, startIndex, endIndex, endDate) {
+  # Preparation for melt
   data_subset <- data[startIndex:endIndex,]
-  names <- data[startIndex:endIndex,1]
-
-  top_picks <- factor(c(names), levels = c(names)) #automatically alphabetizes so you need to avoid it
   
+  # Melt, converts a data.frame from rows of players and columns as dates to:
+  # | Player | variable (Dates) | value (Probabilities) |
   melted_players <- melt(data_subset, id='Player')
   
-  
-  player_names <- melted_players['Player']
 
+  
+  # Creates a color vector, which is then saved to the melt data frame
   col_vector <- rep(NA, nrow(melted_players))
   melted_names <- melted_players[,1]
   for(index in 1:nrow(melted_players)) {
@@ -26,30 +27,33 @@ plot_Nplayers <- function(data, startIndex, endIndex, endDate) {
   melted_players['color'] <- col_vector
   
 
+  # Converts player names from Firstname Lastname to F. Lastname
+  player_names <- melted_players['Player']
   for(i in 1:nrow(player_names)) {
     player_names[i,1] <- convert_name(player_names[i,1])
   }
-  
   melted_players['Player'] <- player_names
   
-  melted_players <<- melted_players
+  # Sends to global environment for inspection if needed.
+  # melted_players <<- melted_players
   
+  # Adjusts x-axis to be further out to accommodate directlabels
   lastDate <- melted_players[nrow(melted_players),2]
   range <-  c(as.Date("2021-12-01"), as.Date(endDate) + 3)
   
   lineplot <- ggplot(data=melted_players, aes(x=as.Date(variable), y=value, group=Player)) +
     geom_line(size=1, color = melted_players$color, group=melted_players$Player) + 
     geom_point(size=2, color = melted_players$color, group=melted_players$Player) +
-    theme_bw() + 
-    theme(panel.grid.minor.x = element_blank()) +
+    theme_bw() + # Removes gray
+    theme(panel.grid.minor.x = element_blank()) + # no minor axis lines
     theme(panel.grid.minor.y = element_blank()) +
-    theme(legend.position="none") +
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1, size = 12)) +
+    theme(legend.position="none") + # no legend
     xlab("") + 
-    ylab("Avg Probability") +
+    ylab("Ensemble Average Probability") +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1, size = 12)) +
     theme(axis.title.y = element_text(size = 14)) + 
     theme(axis.text.y = element_text(size = 12)) + 
-    scale_x_date(date_breaks = "3 days", limits = range) + 
+    scale_x_date(date_breaks = "3 days", limits = range) + # Major axis (x) every three days, limits as set above
     geom_dl(aes(label = Player), color = melted_players$color, group=melted_players$Player, method = list(dl.trans(x = x + 0.2), "last.bumpup", cex = .9))
 
   #These global assignments are for sanity checking and could just as easily be removed.
@@ -57,6 +61,7 @@ plot_Nplayers <- function(data, startIndex, endIndex, endDate) {
   print(lineplot)
   
   ggsave(
+    ### Decomment this first line and comment the second line if you're doing the Avg > 0.1 filter instead of the Max > 0.5 filter
     #filename = paste("Over 0.1 - Top ", startIndex, "-", endIndex, " players.png", sep=""),
     filename = paste("Top ", startIndex, "-", endIndex, " players.png", sep=""),
     plot = lineplot,
