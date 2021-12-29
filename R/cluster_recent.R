fecd439a9ee9272f4fe2dd2962bb285ac953c4fa
rogersheu <rogersheu92@gmail.com>
2021-12-29 15:00:04 -0800

    dynamic clustering

--- R/cluster_recent.R
+++ R/cluster_recent.R
@@ -5,12 +5,18 @@ cluster_recent <- function(data) {
   data <- filter(temp, !is.na(temp[,ncol(temp)]))
   mostrecent <- data[c(1, ncol(data))] # Names and prob
   num_clusters <- round (nrow(data) / 7, digits = 0)
+  iterations <- 0
   while(TRUE) {
+    iterations <- iterations + 1
+    if(iterations > 25) {
+      num_clusters <- num_clusters + 1
+      iterations <- 0
+    }
     clusterresults <- kmeans(mostrecent[,2], num_clusters)
     centers <- sort(clusterresults$centers)
     centers <- sort(clusterresults$centers, decreasing = TRUE)
     clusterresults <- kmeans(mostrecent[,2], num_clusters, centers = centers)
-    if(max(clusterresults$size) < 10 & min(clusterresults$size > 3)) {
+    if(max(clusterresults$size) < 10 & min(clusterresults$size > 4)) {
       clusterresults <<- clusterresults
       return(clusterresults$size)
     }
