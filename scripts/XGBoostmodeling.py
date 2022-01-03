18176cf9433593aa1d2ec306716a1a233a987c8a
rogersheu <rogersheu92@gmail.com>
2022-01-03 14:21:37 -0800

    use XGBoost instead of Gradient Boosting Classifier

--- scripts/XGBoostmodeling.py
+++ scripts/XGBoostmodeling.py
@@ -1,35 +1,39 @@
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
 from transfer_data import *
+
+
+# Most important hyperparameters, according to https://towardsdatascience.com/mastering-xgboost-2eb6bce6bc76
+
+# (1) how many sub-trees to train; 
+# (2) the maximum tree depth (a regularization hyperparameter); 
+# (3) the learning rate; 
+# (4) the L1 (reg_alpha) and L2 (reg_ lambda) regularization rates that determine the extremity of weights on the leaves; 
+# (5) the complexity control (gamma=γ), a pseudo- regularization hyperparameter; 
+# (6) minimum child weight
 
 iterations = 10
 
 def XGBoost(X, y, X_2022):
 
-    prediction_trials = []
-    # No scaling required
-    for i in range(iterations):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify = y)
-        xg = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify = y)
+    xgb_model = xgb.XGBClassifier()
 
-        # Possible hyperparameters
-        # xg = GradientBoostingClassifier(tree_method=hist, grow_policy=lossguide, eta=0.1, gamma=1.0, max_depth=0, max_leaves=255, min_child_weight=100)
+    xgb_model.fit(X_train, y_train)
+    y_pred = xgb_model.predict(X_test)
 
-        xg.fit(X_train, y_train)
-
-        y_pred = xg.predict(X_test)
-
-
-        prediction_trials.append(xg.predict_proba(X_2022)[:,1])
-
-
-    print(f'Confusion matrix and classification report for XGBoost model, final iteration.\n')
+    print('Confusion matrix and classification report for XGBoost model.\n')
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
-    df = pd.DataFrame(prediction_trials).transpose()
 
-    return df.mean(axis=1)
+
+
+    predictions = xgb_model.predict_proba(X_2022)
+
+    return predictions[:,1]
+
 
 def main():
     X, y = get_all_player_stats()
