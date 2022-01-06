7ca5fda4fa2114d24dc8594194d0904553f50094
rogersheu <rogersheu92@gmail.com>
2022-01-05 21:57:41 -0800

    GBM tuning is really slow

--- scripts/GBMtuning.py
+++ scripts/GBMtuning.py
@@ -13,15 +13,17 @@ def gradientboosted_tuning(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify = y)
     
     parameter_space = { 
-        "min_samples_split" : [],
-        "min_samples_leaf" : [],
-        "max_depth" : [],
-        "max_leaf_nodes" : [],
-        "max_features" : [],
-        "n_estimators": []
+        "learning_rate" : [0.01, 0.05, 0.1, 0.25, 0.5],
+        #"min_samples_split" : [],
+        #"min_samples_leaf" : [],
+        "max_depth" : [3, 5, 7, 9],
+        #"max_leaf_nodes" : [],
+        #"max_features" : [],
+        "n_estimators": [50, 100, 200]
     }
     
-    gb_model = GradientBoostingClassifier(max_iter = 2000)
+    # Option to use HistGradientBoostingClassifier
+    gbm_model = GradientBoostingClassifier()
 
     scores = ["precision", "recall"]
 
@@ -29,38 +31,24 @@ def gradientboosted_tuning(X, y):
         print("# Tuning hyper-parameters for %s" % score)
         print()
 
-        clf = GridSearchCV(gb_model, parameter_space, scoring="%s_macro" % score, n_jobs = -1, cv = 3) 
+        clf = GridSearchCV(gbm_model, parameter_space, scoring = "f1") #"%s_macro" % score, cv = 5) 
         clf.fit(X_train, y_train)
 
-        print("Best parameters set found on development set:")
-        print()
+        print("Best parameters set found on development set:\n")
         print(clf.best_params_)
-        print()
-        print("Grid scores on development set:")
-        print()
+        print("\nGrid scores on development set:\n")
         means = clf.cv_results_["mean_test_score"]
-        if score == "precision":
-            precision_means = means
-        else:
-            recall_means = means
         stds = clf.cv_results_["std_test_score"]
+
         for mean, std, params in zip(means, stds, clf.cv_results_["params"]):
             print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
         print()
 
-        print("Detailed classification report:")
-        print()
+        print("Detailed classification report:\n")
         print("The model is trained on the full development set.")
-        print("The scores are computed on the full evaluation set.")
-        print()
+        print("The scores are computed on the full evaluation set.\n")
         y_true, y_pred = y_test, clf.predict(X_test)
         print(classification_report(y_true, y_pred))
-        print()
-
-
-    plt.scatter(precision_means, recall_means)
-    plt.show()
-
 
 def main():
     X, y = get_all_player_stats()
