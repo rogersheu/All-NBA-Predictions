b404d9a6a7dd65d4e80c2aaf85df7b7b9b9bfb03
rogersheu <rogersheu92@gmail.com>
2022-01-06 14:25:50 -0800

    finished tuning

--- scripts/GBMtuning.py
+++ scripts/GBMtuning.py
@@ -8,18 +8,20 @@ import matplotlib.pyplot as plt
 ### Results from different hyperparameter combinations (perhaps randomized) to form an ensemble
 # Wenzel et al. 2020, Hyperparameter Ensembles for Robustness and Uncertainty Quantification, https://arxiv.org/abs/2006.13570
 
+# Incredibly slow, would recommend XGBoost or LightGBM instead
+
 def gradientboosted_tuning(X, y):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify = y)
     
     parameter_space = { 
-        "learning_rate" : [0.01, 0.05, 0.1, 0.25, 0.5],
+        #"learning_rate" : [0.05, 0.1, 0.25, 0.5], # 0.05, 0.1, and 0.25 were all fine. 0.1 is default.
         #"min_samples_split" : [],
         #"min_samples_leaf" : [],
-        "max_depth" : [3, 5, 7, 9],
+        #"max_depth" : [3, 5, 7, 9], # The default of 3 is best
         #"max_leaf_nodes" : [],
         #"max_features" : [],
-        "n_estimators": [50, 100, 200]
+        # "n_estimators": [50, 100, 200] # Score dropped for both 50 and 200
     }
     
     # Option to use HistGradientBoostingClassifier
