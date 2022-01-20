0797c11c67747722f98565bc7f969597ad3e3811
rogersheu <rogersheu92@gmail.com>
2022-01-19 21:50:54 -0800

    modify for the pipeline

--- scripts/SQL/prep_tables_for_extraction.sql
+++ scripts/SQL/prep_tables_for_extraction.sql
@@ -5,6 +5,7 @@
 ALTER TABLE advanced RENAME COLUMN "TS%" TO TS;
 ALTER TABLE advanced RENAME COLUMN "WS/48" TO WS48;
 ALTER TABLE teamStandingsAbbrev RENAME TO teamWL2022;
-ALTER TABLE teamadv_csv RENAME TO teamadv;
+--ALTER TABLE teamadv_csv RENAME TO teamadv;
 ALTER TABLE teamadv RENAME COLUMN "TS%" TO TS;
 DROP TABLE teamStandings;
+DROP TABLE stats;
