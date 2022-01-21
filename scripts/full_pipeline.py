29082434cbd2dbee7c60a3fc69259195161d1b31
rogersheu <rogersheu92@gmail.com>
2022-01-21 11:46:16 -0800

    to use for "today's" data

--- scripts/full_pipeline.py
+++ scripts/full_pipeline.py
@@ -6,10 +6,12 @@ from datetime import date
 date_today = date.today()
 today = date_today.strftime("%Y-%m-%d")
 
+data_path = f'./baseData/dailystats/{today}'
+
 def full_pipeline():
 
-    # daily_data_script()
-    # database_pipeline()
+    daily_data_script()
+    database_pipeline(data_path)
     automated_modeling(today, today)
 
 
