e12f953d47100427262c3d8b3dac61f46ca04e16
rogersheu <rogersheu92@gmail.com>
2022-12-04 15:05:19 -0800

    deleted OOP folder

--- src/OOP/player_accolades.py
+++
@@ -1,5 +0,0 @@
-
-class PlayerAccolades:
-    name: str = ""
-    allstar: bool = False
-    allnba: bool = False
--- src/OOP/player_object.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Player(object):
-    name: str = ""
-    ID: str = ""
-    year: int = 0
